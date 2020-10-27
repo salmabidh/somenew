e910584938a056f9b650acfc5a30678302338480
Abidh123 <abidhsk123@gmail.com>
2020-10-27 11:57:29 +0530

    added set timeout value as 1

--- iot_postgresql.sql
+++ iot_postgresql.sql
@@ -7,7 +7,7 @@
 
 -- Started on 2020-04-16 17:52:32
 
-SET statement_timeout = 0;
+SET statement_timeout = 1;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
